0e3bddcafb5fb1319b89f6d3aec62b63471e0fe3
Xinjie Yao <xyao@nvidia.com>
2025-05-14 14:46:00 -0700

    update license header

--- source/isaaclab_eval_tasks/isaaclab_eval_tasks/tasks/__init__.py
+++ source/isaaclab_eval_tasks/isaaclab_eval_tasks/tasks/__init__.py
@@ -1,8 +1,21 @@
+# SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 """Package containing task implementations for various robotic environments."""
 
 import gymnasium as gym
-import os
-import toml
 
 from isaaclab_tasks.utils import import_packages
 
@@ -11,8 +24,6 @@ from .manipulation.pick_place import exhaustpipe_gr1t2_closedloop_env_cfg, nutpo
 ##
 # Register Gym environments.
 ##
-
-
 gym.register(
     id="Isaac-ExhaustPipe-GR1T2-ClosedLoop-v0",
     entry_point="isaaclab.envs:ManagerBasedRLEnv",
